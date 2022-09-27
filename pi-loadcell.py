bb05ff080faee404d0da2b928255638318743bd0
Trevor Hunter <trevor.j.hunter@outlook.com>
2022-09-26 22:08:54 -0600

    Added functions for reading weights
    Added readWeight() and readWeightString() functions for reading in integer weight value, and formatted string weight value with units suffix respectively. Added scale tareOffset, unitConversion, and unitString parameters to HX711LoadCell class.

--- pi-loadcell.py
+++ pi-loadcell.py
@@ -9,9 +9,12 @@ GPIO.setmode(GPIO.BCM)  # set up BCM GPIO numbering
 class HX711LoadCell:
   
   
-  # pd_sck  | digital input   | GPIO pin for power down control (high active) and serial clock input
-  # dout    | digital output  | GPIO pin for serial data output 
-  def __init__(self, pd_sck, dout, gain=128, bitMSB=True, byteMSB=True, channelSelect='A'):
+  # pd_sck  | digital input           | GPIO pin for power down control (high active) and serial clock input
+  # dout    | digital output          | GPIO pin for serial data output 
+  # gain    | configuration parameter | Gain setting for HX711. Channel A can be programmed with a gain of 128 or 64. Channel B has a fixed gain of 32
+  # bitMSB  | configuration parameter | Sets bit format for reading data from HX711, True for MSB, false for LSB
+  # byteMSB | configuration parameter | Sets byte format for reading data from HX711, True for MSB, false for LSB
+  def __init__(self, pd_sck, dout, gain=128, bitMSB=True, byteMSB=True):
     
     self.pd_sck = pd_sck
     self.dout = dout
@@ -21,8 +24,14 @@ class HX711LoadCell:
     self.bitMSB = bitMSB
     self.byteMSB = byteMSB
     
-    # HX711 has two input channels, A & B
-    self.channelSelect = channelSelect
+    # Set factor for unit conversion of raw value to weight
+    self.unitConversion = 1
+    
+    # Set default offset for scale tare value
+    self.tareOffset = 0
+    
+    # Units suffix for readWeightString function formatting
+    self.unitsString = ""
     
     # Initialize GPIO
     GPIO.setup(self.pd_sck, GPIO.IN)
@@ -120,25 +129,45 @@ class HX711LoadCell:
       startShift = (numBytes * 8) - 8 # For left shift of bits in byte array
       byteValue = 0b0
       
-      for i in range(numBytes)
+      for i in range(numBytes):
       
         byteValue |= (byteArray[i] << startShift)
         startShift -= 8
         
       return byteValue
       
+      
+      
     # Read 3 bytes from DOUT for 24 Bits of data from MX711
     # Returns int value
-    def read24Bit(self):
+    def readRawBytes(self):
       
       # variable for storing read byte data from DOUT
       dataBytes = []
       
       # Read 3 bytes from DOUT and store in array
-      for i in range (3)
+      for i in range (3):
         
         dataBytes.append(self.readByte())
       
+      # Gain setting for HX711 determines number of padding bits after 24 bits of data read on bus
+      #
+      #   | PD_SCK Pulses | Input Channel |  Gain  |
+      #   |---------------+---------------+--------|
+      #   |       25      |       A       |   128  |
+      #   |       26      |       B       |   32   |
+      #   |       27      |       A       |   64   |
+      #
+      pulseOffset = 1
+      
+      if self.gain == 32:
+        pulseOffset = 2
+      elif self.gain == 64:
+        pulseOffset = 3
+      
+      # Clear bits equal to pulse offset determined by gain
+      self.clearDOUT(pulseOffset)
+        
       # If MSB byte format, reverse order of bytes in array
       if self.byteMSB:
           
@@ -147,6 +176,64 @@ class HX711LoadCell:
       return self.compactBytes(dataBytes)
     
     
+    
+    # Read raw value from DOUT serial interface, and convert from twos compliment value to integer value
+    def readRawInt(self):
+      
+      # Read compacted 24 bit sample from DOUT
+      byteData = self.readRawBytes()
+      
+      # Value from HX711 
+      # Two's compliment conversion reference:
+      # https://www.exploringbinary.com/twos-complement-converter/
+      rawValue = -(byteData & 0x800000) + (byteData & 0x7FFFFF)
+      
+      # Return rawValue cast to integer
+      return int(rawValue)
+      
+    
+    
+    # Returns weight value read from HX711
+    def readWeight(self):
+      
+      # Read in raw value
+      rawValue = self.readRawInt()
+      
+      # Offset by current tare value
+      compensatedValue = rawValue - self.tareOffset
+      
+      # Return compensated value divided by unit conversion
+      return compensatedValue / self.unitConversion
+    
+    
+    
+    # Returns formatted weight value as string with units suffix
+    def readWeightString(self, decimalPlaces = 2):
+      
+      # Read in weight value
+      readValue = self.readWeight()
+      
+      # Generate formatted string
+      formatText = "{weight:." + str(decimalPlaces) + "f} " + self.unitsString
+      
+      # Return formatted string with weight value
+      return formatText.format(weight = readValue)
+      
+      
+      
+    # Clears specified number of bits from DOUT
+    def clearDOUT(self, bits=24):
+      
+      # Set PK_SCK low
+      GPIO.output(self.pk_sck, False)
+      
+      # Clear DOUT by reading in passed number of bits of raw data
+      for i in range(bits):
+        
+        self.readBit()
+      
+      
+      
     # When PD_SCK pin changes from low to high 
     # and stays at high for longer than 60µs, HX711 
     # enters power down mode
@@ -164,6 +251,9 @@ class HX711LoadCell:
       
       # Write PK_SCK pin low
       GPIO.output(self.pk_sck, False)
+      
+      # Clear DOUT
+      self.clearDOUT()
       
       
       
